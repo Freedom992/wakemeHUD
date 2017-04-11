6c49e761e6406b3893f23927687314bd984670ec
Anthagonas <the_rod_21@hotmail.com>
2017-04-11 13:00:14 +0200

    Add files via upload

--- wakemehud/MainActivity.java
+++ wakemehud/MainActivity.java
@@ -1,44 +1,107 @@
 package com.example.anthagonas.wakemehud;
 
-import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.TextClock;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
-    protected int scaleY_value = 1; // tentative pour garder le -1 sur toutes les activités
+import java.util.ArrayList;
+
+public class MainActivity <T extends Fragment> extends AppCompatActivity {
+
+    protected ArrayList <T> fragmentList = new ArrayList <T>();
+    protected int fragmentListPosition = 0;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) { // Cree notre activite main avec l'heure
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextClock digital = (TextClock) findViewById(R.id.digital_clock);
+
+        //recuperation de l'etat de la batterie
+        registerReceiver(infos_batterie, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        // Ajout des differents fragments
+        T bob = (T) new Heure();
+        T notif = (T) new Notifications();
+        T meteo = (T) new Meteo();
+        T rss = (T) new Rss();
+        T agenda = (T) new Agenda();
+        T agregateur = (T) new Agregateur();
+
+        fragmentList.add(bob);
+        fragmentList.add(notif);
+        fragmentList.add(meteo);
+        fragmentList.add(rss);
+        fragmentList.add(agenda);
+        fragmentList.add(agregateur);
+
+
+        //Configuration du comportement des boutons
+        Button hud = (Button) findViewById(R.id.hud);
+        hud.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                FrameLayout layout = (FrameLayout) findViewById(R.id.framelayout); // Recupere le framelayout
+                layout.setScaleY(-layout.getScaleY()); // retourne le framelayout ( et donc le fragment affiché)
+            }
+        });
+
+
+        Button suivant = (Button) findViewById(R.id.suivant) ;
+        suivant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                fragmentListPosition = (fragmentListPosition+1)%fragmentList.size(); // se déplace dans la liste des fragments
+                FragmentTransaction fragmentManager= getSupportFragmentManager().beginTransaction();
+                fragmentManager.replace(R.id.framelayout, fragmentList.get(fragmentListPosition)); // Remplace le fragment actuel par le suivant dans la liste
+                fragmentManager.commit();
+            }
+        });
+
+        Button parametres = (Button) findViewById(R.id.parametres);
+        parametres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent myIntent = new Intent(MainActivity.this, Parametres.class); // Chargement de l'activite parametres
+                startActivity(myIntent); // lancement de l'activite
+            }
+        });
+
+        if (savedInstanceState != null)
+        { return;} // la suite du code n'est a appliquer que si l'appli demarre
+
+        FragmentTransaction fragmentManager = getSupportFragmentManager().beginTransaction();
+        fragmentManager.add(R.id.framelayout, fragmentList.get(0)); // affichage du fragment par defaut (premier dans la liste)
+        fragmentManager.commit();
     }
 
-    //Permet de passer d'un activité a une autre TODO : Pour toutes les activités (Que notifications pour le moment)
-    public void changeActivity(View v)
-    {
-        Intent TaskIntent = new Intent(this,Notifications.class);
-        startActivity(TaskIntent);
-    }
-
-    //Contient les methodes utilisables par les bouttons
-    public class ButtonListener implements android.view.View.OnClickListener
-    {
-        public void onClick(View v)
-        {
-            changeActivity(v);
-            mirror(v);
+    //affichage de l'etat de la batterie :
+    private BroadcastReceiver infos_batterie = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent i) {
+            int level = i.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            TextView etat = (TextView) findViewById(R.id.batterie);
+            etat.setText("Niveau de batterie: " + Integer.toString(level) + "%");
         }
+    };
+
+    // Quitter l'appli lors de l'appui sur la touche retour
+    @Override
+    public void onBackPressed()
+    {
+        finish();
     }
 
-    //Permet a un boutton de retourner les elements "Hudable"
-    public void mirror(View v)
-    {
-        View parent = (View) v.getParent();
-        RelativeLayout layout = (RelativeLayout) findViewById(R.id.hudable);
-        layout.setScaleY(-layout.getScaleY());
-        scaleY_value -= scaleY_value;
-    }
 }
